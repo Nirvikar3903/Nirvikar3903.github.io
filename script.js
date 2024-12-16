62fed76967fc5a1c4927ed759aed3c59adf98a26
Nirvikar3903 <nirvikar03.k@gmail.com>
2024-12-16 13:09:12 +0530

    adding HTML and css files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// // 1. Function for downloading resume
+// document.querySelector(".button1").addEventListener("click", function () {
+//   // Use the download attribute with an anchor tag to trigger a download
+//   const link = document.createElement('a');
+//   link.href = 'path/to/your/resume; // Add your actual resume file path here
+//   link.download = file:///C:/Users/HP/Downloads/PUnit%20jadhav%20(2).pdf;
+//   link.click();
+// });
